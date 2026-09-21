9bc94ff3aaed74a75bc5caf4b6cd02f94f4ed72d
yoshida1126 <wwstnugzvctc6qpg1i1s@docomo.ne.jp>
2026-09-21 12:42:25 +0900

    add local file

---
+++ themes/Kids-U/index.php
@@ -0,0 +1,197 @@
+<?php get_header(); ?>   
+  <section id="mainvisual">
+    <div class="catchphrase">
+      <p>「好き」が未来を</p>
+      <p>連れてくる</p>
+    </div>
+    <div class="garland">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_garland1.png">
+    </div>
+    <div class="ball">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_ball.png">
+    </div>
+    <div class="crayon">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_crayon.png">
+    </div>
+  </section>
+  <section id="feature" class="wrapper">
+    <h2>
+      <span>Kids-Uが選ばれる理由</span>  
+    </h2>
+    <ul class="feature-list">
+      <li>
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/features_choose.jpg" alt="跳んでいる男の子">
+        <h3>知・スポーツ・芸術の習い事が選び放題</h3>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      </li>
+      <li>
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/features_pace.jpg" alt="笑っている男の子">
+        <h3>お子様のペースに合わせて</h3>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      </li>
+      <li>
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/features_building.jpg" alt="建物">
+        <h3>同じ建物内で全ての習い事を実施</h3>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      </li>
+    </ul>
+    <a href="#" class="btn">体験申し込み</a>
+    <div class="sheep">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_sheep.png">
+    </div>
+    <div class="penguin">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_penguin.png">
+    </div>
+    <div class="wave-border">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+  </section>
+  <section id="course" class="wrapper">
+    <h2>
+      <span>コース案内</span>
+    </h2>
+    <div class="course1">
+      <div class="course-img">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/programming.jpg" alt="パソコンを見ている二人の男の子">
+      </div>
+      <div class="text">
+        <h3>知育・学習コース</h3>
+        <p>英語・プログラミング・そろばん</p>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        <a href="#" class="course-btn">詳しく見る</a>
+      </div>
+    </div>
+    <div class="course2">
+      <div class="course-img">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/futsal.jpg" alt="フットサルをしている子どもたち">
+      </div>
+      <div class="text">
+        <h3>体育・スポーツコース</h3>
+        <p>体操・フットサル・バレエ＆新体操</p>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        <a href="#" class="course-btn">詳しく見る</a>
+      </div>
+    </div>
+    <div class="course3">
+      <div class="course-img">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/art.jpg" alt="絵を描く男の子">
+      </div>
+      <div class="text">
+        <h3>芸術・音楽コース</h3>
+        <p>絵画・習字・ピアノ＆エレクトーン</p>
+        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        <a href="#" class="course-btn">詳しく見る</a>
+      </div>
+    </div>
+    <a href="#" class="btn">体験申し込み</a>
+    <div class="course-garland1">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_garland1.png">
+    </div>
+    <div class="book">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_scrapbook.png">
+    </div>
+    <div class="flower">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_gerbera.png">
+    </div>
+    <div class="course-garland2">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_garland1.png">
+    </div>
+  </section>
+  <section id="news" class="wrapper">
+    <div class="wave-border">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <div class="news-container">
+      <h2>
+        <span>新着情報</span>
+      </h2>
+      <dl>
+        <a href="#">
+          <dt>
+            <time datetime="2026-09-13">⚪︎⚪︎年⚪︎月⚪︎日</time>
+          </dt>
+          <dd>
+            ６月１日～６日まで、施設の水道管工事の為臨時休業いたします。
+          </dd>
+        </a>
+        <a href="#">
+          <dt>
+            <time datetime="2026-09-13">⚪︎⚪︎年⚪︎月⚪︎日</time>
+          </dt>
+          <dd>
+            ６月１日～６日まで、施設の水道管工事の為臨時休業いたします。
+          </dd>
+        </a>
+        <a href="#">
+          <dt>
+            <time datetime="2026-09-13">⚪︎⚪︎年⚪︎月⚪︎日</time>
+          </dt>
+          <dd>
+            ６月１日～６日まで、施設の水道管工事の為臨時休業いたします。
+          </dd>
+        </a>
+        <a href="#">
+          <dt>
+            <time datetime="2026-09-13">⚪︎⚪︎年⚪︎月⚪︎日</time>
+          </dt>
+          <dd>
+            ６月１日～６日まで、施設の水道管工事の為臨時休業いたします。
+          </dd>
+        </a>
+      </dl>
+      <a href="#" class="btn">もっと見る</a>
+    </div>
+    <div class="hamster">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_hamster.png">
+    </div>
+    <div class="hyacinth">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_hyacinth.png">
+    </div>
+    <div class="bottom-wave-border">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+  </section>
+  <section id="access" class="wrapper">
+    <h2>
+      <span>アクセス</span>
+    </h2>  
+    <div class="access-container">
+      <div class="information">
+        <p>幼児教室</p>
+        <p>Kids-U</p>
+        <dl>
+          <dt>営業時間</dt>
+          <dd>平日 9：00～17：00（土日祝 9:00~19:00）</dd>
+
+          <dt>住所</dt>
+          <dd>〒123-4567　東京都小松区小松１－２－３</dd>
+
+          <dt>TEL</dt>
+          <dd>000-123-4567（FAX 123-456-8888）</dd>
+
+          <dt>E-mail</dt>
+          <dd>kidsuuu@freemail.com</dd>
+
+          <dt>駐車場有</dt>
+          <dd>※保護者様専用5台。満車の際はお近くの有料駐車場をご利用下さい。</dd>
+        </dl>
+      </div>
+      <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8330566915583!2d139.76451597623134!3d35.681112429979784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1718744341688!5m2!1sja!2sjp" width="800" height="800" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+    <a href="#" class="btn">体験申し込み</a>
+    <div class="access-garland">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_garland1.png">
+    </div>
+    <div class="slide">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/pop_slide.png">
+    </div>
+  </section>
+<?php get_footer(); ?>
